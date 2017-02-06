b7ab5f2c64555eb43f1cc6e70bc01e62539fc075
Joseph Noel <noelj@rpi.edu>
2017-02-06 17:33:04 -0500

    Added mantisBT bug tracking

---
+++ mantisbt/config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = '127.0.0.1';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = 'root';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'dwusKRZNX72l6BiAUkYSgtk2+Aav9xqAhpYiLIvH6dQ=';
